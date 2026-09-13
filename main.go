fcd87afb49232c455fe6cbfe0116409436dce378
jonasssneto <nogueiraneto.jonas@gmail.com>
2026-09-13 16:07:05 -0300

    feat: add dashboard entrypoint

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	tea "charm.land/bubbletea/v2"
+	"clocky/internal/ui"
+)
+
+func main() {
+	program := tea.NewProgram(ui.NewModel())
+	if _, err := program.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
